bbaa6c07e00ddccca05a0eea3670b357a1f7d575
dantegamesalex <dantegamesalex@gmail.com>
2021-07-20 16:18:59 +0300

    Добавлен сервис для получения отфильтрованного списка по названию

---
+++ MVVMTestBM/Services/FiltrationService.cs
@@ -0,0 +1,31 @@
+﻿using CompletedMVVMTestBM.Services.Interfaces;
+using MVVMTestBM.Models.Interfaces;
+using MVVMTestBM.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompletedMVVMTestBM.Services
+{
+    public class FiltrationService : IFiltrationService
+    {
+        private BookRepository _bookRepository;
+
+        public FiltrationService(BookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public IEnumerable<IBook> GetBooksByName(string name)
+        {
+            if (name == "" || string.IsNullOrWhiteSpace(name))
+            {
+                return _bookRepository.Books;
+            }
+
+            return _bookRepository.Books.Where(b => b.Name.ToUpper().StartsWith(name.ToUpper()));
+        }
+    }
+}
---
+++ MVVMTestBM/Services/Interfaces/IFiltrationService.cs
@@ -0,0 +1,14 @@
+﻿using MVVMTestBM.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompletedMVVMTestBM.Services.Interfaces
+{
+    public interface IFiltrationService
+    {
+        public IEnumerable<IBook> GetBooksByName(string name);
+    }
+}
